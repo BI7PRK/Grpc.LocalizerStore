1ccf6d5b2f816fd1b51b3ab934a3143e1f52c703
BI7PRK <im@bi7prk.com>
2025-04-03 17:56:00 +0800

    Fixed: AddStringLocalizerStore

--- Service/StringLocalizerStore.cs
+++ Service/StringLocalizerStore.cs
@@ -3,6 +3,7 @@ using GoI18n;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 
@@ -31,27 +32,40 @@ namespace AspNetCore.Grpc.LocalizerStore.Service
     }
     public class StringLocalizerStore : IStringLocalizerStore
     {
+        readonly Dictionary<string, ReadOnlyDictionary<string, string>> _localizerCache = new Dictionary<string, ReadOnlyDictionary<string, string>>();
+
         readonly ReadOnlyDictionary<string, string> _resources;
         public StringLocalizerStore(ILocalizerChannel i18NChannel, ILogger<StringLocalizerStore> logger)
         {
-            logger.LogInformation("code: {0}", CultureInfo.CurrentCulture.Name);
+            var code = CultureInfo.CurrentCulture.Name;
+            logger.LogInformation("code: {0}", code);
+            if (_localizerCache.TryGetValue(code, out var localizer))
+            {
+                _resources = localizer;
+                return;
+            }
             var resources = new Dictionary<string, string>();
-            var res = i18NChannel.Client.GetCultureResources(new CultureCodeRequest
+            try
             {
-                Code = CultureInfo.CurrentCulture.Name
-            });
-            if (res.Code == ReplyCode.Success)
-            {
-                foreach (var item in res.Items)
+                var res = i18NChannel.Client.GetCultureResources(new CultureCodeRequest
                 {
-                    resources.Add(item.Key, item.Text);
+                    Code = code
+                });
+                if (res.Code == ReplyCode.Success)
+                {
+                    foreach (var item in res.Items)
+                    {
+                        resources.Add(item.Key, item.Text);
+                    }
+                    _localizerCache[code] = new ReadOnlyDictionary<string, string>(resources);
                 }
-                _resources = new ReadOnlyDictionary<string, string>(resources); ;
+                else
+                {
+                    logger.LogError("GetCultureResources failed: {0}", res.Message);
+                }
             }
-            else
-            {
-                logger.LogError("GetCultureResources failed: {0}", res.Message);
-            }
+            catch {  }
+            _resources = new ReadOnlyDictionary<string, string>(resources); ;
         }
 
         public string GetString(string name)
@@ -96,7 +110,7 @@ namespace AspNetCore.Grpc.LocalizerStore.Service
         /// <returns></returns>
         public static IServiceCollection AddStringLocalizerStore(this IServiceCollection services)
         {
-            services.TryAddSingleton<IStringLocalizerStore, StringLocalizerStore>();
+            services.TryAddScoped<IStringLocalizerStore, StringLocalizerStore>();
             return services;
         }
     }
