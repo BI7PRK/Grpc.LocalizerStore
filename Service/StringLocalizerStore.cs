ea9f12315991bd31922182fb56dea20830fbe5a9
BI7PRK <im@bi7prk.com>
2025-04-03 18:24:56 +0800

    Add UsRequestLocalizatioStore()

--- Service/StringLocalizerStore.cs
+++ Service/StringLocalizerStore.cs
@@ -1,5 +1,7 @@
 ﻿using AspNetCore.Grpc.LocalizerStore.Rpc;
 using GoI18n;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
@@ -23,20 +25,25 @@ namespace AspNetCore.Grpc.LocalizerStore.Service
         /// </summary>
         /// <returns></returns>
         ReadOnlyDictionary<string, string> GetAllStrings();
+        Task<CultureItem[]> GetCultures();
+
         /// <summary>
         /// 获取本地化资源
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
         string GetString(string name);
+        void Reload(string culture);
+        void ReloadAll();
     }
     public class StringLocalizerStore : IStringLocalizerStore
     {
         readonly Dictionary<string, ReadOnlyDictionary<string, string>> _localizerCache = new Dictionary<string, ReadOnlyDictionary<string, string>>();
-
         readonly ReadOnlyDictionary<string, string> _resources;
+        readonly ILocalizerChannel _i18NChannel;
         public StringLocalizerStore(ILocalizerChannel i18NChannel, ILogger<StringLocalizerStore> logger)
         {
+            _i18NChannel = i18NChannel;
             var code = CultureInfo.CurrentCulture.Name;
             logger.LogInformation("code: {0}", code);
             if (_localizerCache.TryGetValue(code, out var localizer))
@@ -110,10 +117,45 @@ namespace AspNetCore.Grpc.LocalizerStore.Service
         {
             return _resources;
         }
+
+        public async Task<CultureItem[]> GetCultures()
+        {
+            try
+            {
+                var res = await _i18NChannel.Client.CultureFeatureAsync(new CulturesRequest
+                {
+                    Action = ActionTypes.List
+                });
+                if (res.Code == ReplyCode.Success)
+                {
+                  return res.Items.ToArray();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Array.Empty<CultureItem>();
+        }
+
         /// <summary>
         /// 重载本地化资源
         /// </summary>
-        public void Reload() => _localizerCache.Clear();
+        public void ReloadAll() => _localizerCache.Clear();
+
+        /// <summary>
+        /// /// 重载本地化资源
+        /// </summary>
+        /// <param name="culture"></param>
+        public void Reload(string culture)
+        {
+            if (_localizerCache.ContainsKey(culture))
+            {
+                _localizerCache.Remove(culture);
+            }
+        }
+
     }
 
     public static class StringLocalizerStoreExtensions
@@ -128,5 +170,31 @@ namespace AspNetCore.Grpc.LocalizerStore.Service
             services.TryAddScoped<IStringLocalizerStore, StringLocalizerStore>();
             return services;
         }
+
+        /// <summary>
+        /// /// 添加本地化资源服务
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UsRequestLocalizatioStore(this IApplicationBuilder app)
+        {
+            var localizerStore = app.ApplicationServices.GetService<IStringLocalizerStore>();
+            if (localizerStore != null)
+            {
+                var culture = CultureInfo.CurrentCulture.Name;
+                var resources = localizerStore.GetCultures().GetAwaiter().GetResult();
+
+                var supportedCultures = resources.Select(s => new CultureInfo(s.Code)).ToArray();
+                var defaultCulture = resources.FirstOrDefault(w=>w.IsDefault)?.Code ?? CultureInfo.CurrentCulture.Name;
+                app.UseRequestLocalization(new RequestLocalizationOptions
+                {
+                    DefaultRequestCulture = new RequestCulture(defaultCulture),
+                    SupportedCultures = supportedCultures,
+                    SupportedUICultures = supportedCultures
+                });
+            }
+            return app;
+        }
     }
+
 }
